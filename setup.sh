b4afc9e088f9b10e8a245a9a5f8ab82fc92eccde
Hanwn <hw@hanwn.dev>
2021-11-04 15:09:12 +0800

    add git config

--- setup.sh
+++ setup.sh
@@ -1,15 +1,37 @@
-#/bin/bash
+#!/bin/bash
 
 nvim_link=~/.config/nvim
+git_link=${HOME}/.gitconfig
+
 
 function make_nvim_link() {
-    if [ -L ${nvim_link} ]; then
-        rm ${nvim_link}
+    if [ -e ${nvim_link} ]
+    then
+        if [ ! -L ${nvim_link} ]
+        then
+            rm ${nvim_link}
+            ln -s ${HOME}/dotfiles/nvim ${nvim_link}
+        fi
+    else
+        ln -s $HOME/dotfiles/nvim $HOME/.config/nvim
     fi
-    ln -s $HOME/dotfiles/nvim $HOME/.config/nvim
+
+}
+
+function make_git_link() {
+    if [ -e ${git_link} ]
+    then
+        if [ ! -L ${git_link} ]
+        then
+            rm ${git_link}
+            ln -s ${HOME}/dotfiles/git/gitconfig ${git_link}
+        fi
+    else
+        ln -s $HOME/dotfiles/nvim $HOME/.config/nvim
+    fi
 }
 
 
 
-
 make_nvim_link
+make_git_link
