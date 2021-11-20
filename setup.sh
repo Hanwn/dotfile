1d2bc872fab2d72c0f1ec6d8cc4c95c76b7dad03
Hanwn <hanwn7721@gmail.com>
2021-11-20 19:28:18 +0800

    :recycle: refactor setup.sh

--- setup.sh
+++ setup.sh
@@ -1,37 +1,4 @@
 #!/bin/bash
 
-nvim_link=~/.config/nvim
-git_link=${HOME}/.gitconfig
-
-
-function make_nvim_link() {
-    if [ -e ${nvim_link} ]
-    then
-        if [ ! -L ${nvim_link} ]
-        then
-            rm ${nvim_link}
-            ln -s ${HOME}/dotfiles/nvim ${nvim_link}
-        fi
-    else
-        ln -s $HOME/dotfiles/nvim $HOME/.config/nvim
-    fi
-
-}
-
-function make_git_link() {
-    if [ -e ${git_link} ]
-    then
-        if [ ! -L ${git_link} ]
-        then
-            rm ${git_link}
-            ln -s ${HOME}/dotfiles/git/gitconfig ${git_link}
-        fi
-    else
-        ln -s $HOME/dotfiles/git/gitconfig $HOME/.gitconfig
-    fi
-}
-
-
-
-make_nvim_link
-make_git_link
+pip install -r ./requirements.txt
+python ./setup.py
