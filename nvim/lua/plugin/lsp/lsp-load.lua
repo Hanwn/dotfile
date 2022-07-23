51564f65e0eae63fe27ac8441d20f6f1bfc5ca0f
Hanwn <hanwn7721@gmail.com>
2022-07-23 14:55:43 +0800

    add new lsp config

--- nvim/lua/plugin/lsp/lsp-load.lua
+++ nvim/lua/plugin/lsp/lsp-load.lua
@@ -1,20 +1,58 @@
   -- Setup lspconfig.
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', '<leader>K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+end
+
+
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 require('lspconfig')['gopls'].setup {
-capabilities = capabilities
+    on_attach = on_attach,
+    capabilities = capabilities
 }
 require('lspconfig')['clangd'].setup {
-capabilities = capabilities
+    on_attach = on_attach,
+    capabilities = capabilities
 }
 require('lspconfig')['pyright'].setup {
-capabilities = capabilities
+    on_attach = on_attach,
+    capabilities = capabilities
 }
 require('lspconfig')['sumneko_lua'].setup {
-capabilities = capabilities
+    on_attach = on_attach,
+    capabilities = capabilities
 }
 
 require('lspconfig')['bashls'].setup {
-capabilities = capabilities
+    on_attach = on_attach,
+    capabilities = capabilities
 }
 
+require('lspconfig')['rust_analyzer'].setup {
+    on_attach = on_attach,
+}
+
+require('lspconfig')['cmake'].setup {
+    on_attach = on_attach,
+    capabilities=capabilities
+}
