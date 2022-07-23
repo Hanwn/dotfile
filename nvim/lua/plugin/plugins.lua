576479255667745a70b394ba8702f82de6a29078
Hanwn <hanwn7721@gmail.com>
2022-07-23 13:34:56 +0800

    :sparkles: introduce new plugin:project_nvim

--- nvim/lua/plugin/plugins.lua
+++ nvim/lua/plugin/plugins.lua
@@ -58,6 +58,10 @@ return packer.startup(function(use)
     }
 
     use {
+        "ahmedkhalf/project.nvim",
+    }
+
+    use {
         'kyazdani42/nvim-tree.lua',
         requires = {
           'kyazdani42/nvim-web-devicons', -- optional, for file icon
