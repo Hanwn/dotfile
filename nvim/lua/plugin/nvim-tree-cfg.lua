86fd3164dd1ae9fc8748b834ac056e50b4408cdf
Hanwn <hanwn7721@gmail.com>
2022-07-23 13:35:27 +0800

    remap key for nvim-tree

--- nvim/lua/plugin/nvim-tree-cfg.lua
+++ nvim/lua/plugin/nvim-tree-cfg.lua
@@ -1,4 +1,16 @@
 require("nvim-tree").setup{
-    vim.api.nvim_set_keymap('n', '<leader>t', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>t', ':NvimTreeToggle<CR>', { noremap = true, silent = true }),
+    view = {
+        mappings = {
+            list = {
+                {key = 'l', action="edit"},
+                {key = '?', action="toggle_help"},
+                {key = 'h', action="close_node"},
+                {key = 't', action="tabnew"},
+                {key = '[g', action="prev_git_item"},
+                {key = ']g', action="next_git_item"}
+            }
+        }
+    }
 }
 
